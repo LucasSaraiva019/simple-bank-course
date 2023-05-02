2e3b95ffc360707f26cf4a9951036593d8c3234a
Lucas Saraiva <lucas.saraiva019@gmail.com>
2023-05-02 18:23:31 -0300

    addding transfer transaction

---
+++ db/sqlc/store.go
@@ -0,0 +1,88 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
+
+func (store *Store) executeTransaction(ctx context.Context, fn func(*Queries) error) error {
+	transaction, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	q := New(transaction)
+	err = fn(q)
+	if err != nil {
+		if rollbackErr := transaction.Rollback(); rollbackErr != nil {
+			return fmt.Errorf("transaction Err: %v, rollback Err: %v", err, rollbackErr)
+		}
+	}
+	return transaction.Commit()
+}
+
+// TransferTxParams contains the input parameters of the transfer transaction
+type TransferTransactionParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+// TransferTxResult is the result of the transfer transaction
+type TransferTransactionResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+}
+
+func (store *Store) TransferTransaction(ctx context.Context, arg TransferTransactionParams) (TransferTransactionResult, error) {
+	var result TransferTransactionResult
+
+	err := store.executeTransaction(ctx, func(q *Queries) error {
+		var err error
+
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount:    -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount:    arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	return result, err
+}
---
+++ db/sqlc/store_test.go
@@ -0,0 +1,93 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestTransferTransaction(t *testing.T) {
+	store := NewStore(testDB)
+
+	fromAccount := createRadomAccount(t)
+	toAccount := createRadomAccount(t)
+
+	n := 5
+	amount := int64(10)
+
+	errs := make(chan error)
+	results := make(chan TransferTransactionResult)
+
+	for i := 0; i < n; i++ {
+		go func() {
+			result, err := store.TransferTransaction(context.Background(), TransferTransactionParams{
+				FromAccountID: fromAccount.ID,
+				ToAccountID:   toAccount.ID,
+				Amount:        amount,
+			})
+
+			errs <- err
+			results <- result
+		}()
+	}
+
+	existed := make(map[int]bool)
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+
+		result := <-results
+		require.NotEmpty(t, result)
+
+		transfer := result.Transfer
+		require.NotEmpty(t, transfer)
+		require.Equal(t, fromAccount.ID, transfer.FromAccountID)
+		require.Equal(t, toAccount.ID, transfer.ToAccountID)
+		require.Equal(t, amount, transfer.Amount)
+		require.NotZero(t, transfer.ID)
+		require.NotZero(t, transfer.CreatedAt)
+
+		_, err = store.GetTransfer(context.Background(), transfer.ID)
+		require.NoError(t, err)
+
+		fromEntry := result.FromEntry
+		require.NotEmpty(t, fromEntry)
+		require.Equal(t, fromAccount.ID, fromEntry.AccountID)
+		require.Equal(t, -amount, fromEntry.Amount)
+		require.NotZero(t, fromEntry.ID)
+		require.NotZero(t, fromEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), fromEntry.ID)
+		require.NoError(t, err)
+
+		toEntry := result.ToEntry
+		require.NotEmpty(t, toEntry)
+		require.Equal(t, toAccount.ID, toEntry.AccountID)
+		require.Equal(t, amount, toEntry.Amount)
+		require.NotZero(t, toEntry.ID)
+		require.NotZero(t, toEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), toEntry.ID)
+		require.NoError(t, err)
+
+		resFromAccount := result.FromAccount
+		require.NotEmpty(t, resFromAccount)
+		require.Equal(t, fromAccount.ID, resFromAccount.ID)
+
+		resToAccount := result.ToAccount
+		require.NotEmpty(t, resToAccount)
+		require.Equal(t, fromAccount.ID, resToAccount.ID)
+
+		diff1 := fromAccount.Balance - resFromAccount.Balance
+		diff2 := resToAccount.Balance - toAccount.Balance
+		require.Equal(t, diff1, diff2)
+		require.True(t, diff1 > 0)
+		require.True(t, diff1%amount == 0)
+
+		k := int(diff1 / amount)
+		require.True(t, k >= 1 && k <= n)
+		require.NotContains(t, existed, k)
+		existed[k] = true
+	}
+}
